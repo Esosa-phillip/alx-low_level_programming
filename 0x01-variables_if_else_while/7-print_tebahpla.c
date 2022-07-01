06b56b7dead406c69baac62de01e62695769a6a1
Esosa Omoigui <esosa.phillip@yahoo.com>
2022-07-01 14:40:20 -0700

    Alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in reverse.
+ *
+ * Return: Always (0) success.
+ */
+int main(void)
+{
+	int a;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
