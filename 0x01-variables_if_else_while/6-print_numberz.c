485adf13b2504ed0d7548a73eef4a2fbeebaab17
Esosa Omoigui <esosa.phillip@yahoo.com>
2022-07-01 14:34:16 -0700

    Printing numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers from 0 to 10 without char function.
+ *
+ * Return: Always (0) success.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
