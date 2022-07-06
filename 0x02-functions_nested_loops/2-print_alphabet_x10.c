b92d540f7c9ce6b9b98d6c0ad0964f12c4c85304
Esosa Omoigui <esosa.phillip@yahoo.com>
2022-07-06 13:18:22 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int a, b;
-	
-	for (a = 0; a <= 9; a++)
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
 	{
-		for (b = 97; b <= 122; b++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
-			_putchar(b);
+			_putchar(c);
 		}
 		_putchar('\n');
 	}
