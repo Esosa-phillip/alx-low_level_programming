a2b4769971ec22ff6f7b333301be6b2204598a02
Esosa Omoigui <esosa.phillip@yahoo.com>
2022-07-07 10:48:47 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit: checks for digit form 0 through 9.
+ * @c: input parameter.
+ *
+ * Return: 1 if is a number (0 to 9), 0 in other case.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		retuen (0);
+	}
+}
