b35bf13addb34899588b8ec00ec842398ce624de
Esosa <esosa.phillip@yahoo.com>
2022-07-26 16:25:27 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,6 +16,12 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
+	s = malloc(sizeof(int *) * height);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
 	for (r = 0; r < height; r++)
 	{
 		s[r] = malloc(sizeof(int) * width);
