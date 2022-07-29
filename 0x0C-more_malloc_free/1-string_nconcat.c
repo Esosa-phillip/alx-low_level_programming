b7dbe64d642e23a79cf5dbea4f7b97c9a69b9877
Esosa <esosa.phillip@yahoo.com>
2022-07-29 14:22:08 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,61 +6,41 @@
  * @s1: first string.
  * @s2: second string.
  * @n: amount of bytes.
- * 
  * Return: pointer to the allocated memory.
  * if malloc fails, status value is equal to 98.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *south;
-    unsigned int as1, as2, asouth, j;
+	char *south;
+	unsigned int as1, as2, asouth, j;
 
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
+	for (as1 = 0; s1[as1] != '\0'; as1++)
+		;
+	for (as2 = 0; s2[as2] != '\0'; as2++)
+		;
 
-    for (as1 = 0; s1[as1] != '\0'; as1++)
-    {
-        ;
-    }
+	if (n > as2)
+		n = as2;
 
-    for (as2 = 0; s2[as2] != '\0'; as2++)
-    {
-        ;
-    }
+	asouth = as1 + n;
 
-    if (n > as2)
-    {
-        n = as2;
-    }
+	south = malloc(asouth + 1);
 
-    asouth = as1 + n;
+	if (south == NULL)
+		return (NULL);
 
-    south = malloc(asouth + 1);
+	for (j = 0; j < asouth; j++)
+		if (j < as1)
+			south[j] = s1[j];
+		else
+			south[j] = s2[j - as1];
 
-    if (south == NULL)
-    {
-        return (NULL);
-    }
+	south[j] = '\0';
 
-    for (j = 0; j < asouth; j++)
-    {
-        if (j < as1)
-        {
-            south[j] = s1[j];
-        }
-        else
-        {
-            south[j] = s2[j - as1];
-        }
-    }
-    south[j] = '\0';
-
-    return (south);
+	return (south);
 }
