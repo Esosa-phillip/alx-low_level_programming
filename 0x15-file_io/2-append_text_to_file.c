4148a8bbbb86b414cdd98311cc959601d2d3a8f8
Esosa <esosa.phillip@yahoo.com>
2022-08-22 18:18:57 -0700

    [Update] Function that appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (nlet = 0; text_content[nlet], nlet++)
+		for (nlet = 0; text_content[nlet]; nlet++)
 			;
 
 		rwr = write(fd, text_content, nlet);
