a18c22ae897cf1bcd021943dc6686ac6d9da6521
Esosa <esosa.phillip@yahoo.com>
2022-10-10 04:06:02 -0700

    Advanced Task 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Esosa-Phillip/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+wget -P /tmp/ https://raw.github.com/Esosa-Phillip/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/libmask.so
