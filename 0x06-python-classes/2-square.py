1e816432991e6f0d0f160e846861867419d0729c
Esosa <esosa.phillip@yahoo.com>
2022-10-18 02:47:45 -0700

    Size Validation of square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes a new square.
+        Args:
+        size(int)=0: the optional size of a new square."""
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
