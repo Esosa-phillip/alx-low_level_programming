33894fb4e3241adddd324f6df2ed93263691f73f
Esosa <esosa.phillip@yahoo.com>
2022-10-19 13:40:51 -0700

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes a new square
+        Args:
+        size(int): the size of a new square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get the current size of quare"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = 
+
+    def area(self):
+        """Returns the current area of the square."""
+        return (self.__size * self.__size)
