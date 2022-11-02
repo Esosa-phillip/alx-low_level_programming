8e7f4294297003bf9148742482575d75f3357eb5
Esosa Omoigui <esosa.phillip@yahoo.com>
2022-11-02 20:48:47 +0100

    Prints a square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module is composed by a function to print a square with a character #
+"""
+
+def print_square(size):
+    """ Function that prints a square with a character #
+    
+    Args:
+        size: size of square printed
+        
+    Return:
+        No return
+        
+    Raises:
+        TypeError: If size is not an integer
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
