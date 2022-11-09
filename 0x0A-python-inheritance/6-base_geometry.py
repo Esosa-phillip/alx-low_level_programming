eed6a3f53881a327c5845e66ac5d33b90faf4341
Esosa <esosa.phillip@yahoo.com>
2022-11-09 11:38:47 -0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Empty Class"""
+    def area(self):
+        raise Exception("area() is not implemented")
