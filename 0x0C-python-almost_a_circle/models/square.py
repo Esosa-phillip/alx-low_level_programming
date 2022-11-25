04037c813dcdbbb8060603f996f80ac02d48107d
Esosa Omoigui <esosa.phillip@yahoo.com>
2022-11-26 00:31:01 +0100

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,3 +11,12 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes instances"""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string representation of a square"""
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_square + str_id + str_xy + str_wh
