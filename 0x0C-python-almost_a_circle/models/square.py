4c1a466a9a97781951434b84e7c186ddfd3c9083
Esosa Omoigui <esosa.phillip@yahoo.com>
2022-11-25 23:20:16 +0100

    And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that contains class Square,
+inheritance of class Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines the class Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes instances"""
+        super().__init__(size, size, x, y, id)
